18a9c8439b7b93d2a4b795e92acbe5747dc5d982
oojah <97760326+oojah@users.noreply.github.com>
2022-01-21 17:00:04 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <AJ Costanzo>
+ *        Date: <January 21st, 2022>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /cout << "Hello Git!!" <<end1;/
+    return 0;
+}// main
